460a11e5651f2b164dbfbe9f11229955a352cc0a
fajardo2204 <juanfa_2204@hotmail.es>
2025-07-07 13:19:14 -0500

    API creada desde 0 para desarrollarla nosotros mismos + modelos SQL para base de datos

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -1,21 +1,21 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.Models
 {
-    [Table("Comments")]
     public class Comment
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Foreign key
         public int? StockId { get; set; }
+        // Navigation property
         public Stock? Stock { get; set; }
-        public string AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
     }
 }
