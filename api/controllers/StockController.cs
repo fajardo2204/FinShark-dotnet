e38d781e99ee7d1d7bf518010e7ee8f8c999559c
fajardo2204 <juanfa_2204@hotmail.es>
2025-07-14 21:22:54 -0500

    Controlador de Stock creado, primer endpoint Get en desarrollo

---
+++ api/controllers/StockController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.controllers
+{
+    // Attributes
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        // Constructor
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+    }
+}
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460a11e5651f2b164dbfbe9f11229955a352cc0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3797db9e67503390f76472419e7c939fd8dee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
