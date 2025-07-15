f18dc57dae59f2eb89b7476e6cd6e42f1627512b
fajardo2204 <juanfa_2204@hotmail.es>
2025-07-14 21:49:13 -0500

    Endpoint para ver stock individualmente

--- api/controllers/StockController.cs
+++ api/controllers/StockController.cs
@@ -19,11 +19,26 @@ namespace api.controllers
             _context = context;
         }
 
+        // List of actions
         [HttpGet]
         public IActionResult GetAll()
         {
             var stocks = _context.Stocks.ToList();
             return Ok(stocks);
+        }
+
+        // Details of a specific stock
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound(new { message = "Stock not found" });
+            }
+
+            return Ok(stock);
         }
     }
 }
